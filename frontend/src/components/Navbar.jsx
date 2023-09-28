95e3ab0bff5222c9a00899237a5f4f1ea24ce4b6
humayuntariq98 <hammytar23@gmail.com>
2023-09-28 16:04:20 -0400

    add bootstrap 5 card

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,25 +4,19 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
   <div className="container-fluid">
-    <Link className="navbar-brand"to="/">Navbar</Link>
+    <Link className="navbar-brand fs-1 fst-italic"to="/">Bitemate</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page"to="#">Home</Link>
+          <Link className="nav-link active" aria-current="page"to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link"to="#">Features</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link"to="#">Pricing</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link disabled"to="#" tabindex="-1" aria-disabled="true">Disabled</Link>
+          <Link className="nav-link"to="/login">Login</Link>
         </li>
       </ul>
     </div>
